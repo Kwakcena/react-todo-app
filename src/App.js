9938fe90c46a95c3f1a1995490d7d7b8500c1a67
Kwakcena <rhkrgudwh@gmail.com>
2020-03-20 20:15:24 +0900

    11.6 불변성의 중요성
    useReducer 를 사용하는 코드에서 useState 를 사용하는 코드로 바꿨다.
    
    리액트 컴포넌트에서 상태를 업데이트 할 때 불변성을 유지하는 것은 굉장히
    중요하다.
    
    onToggle 함수를 보면, 기존 데이터를 직접 수정하지 않고, 새로운 배열을
    만든 다음 새로운 객체를 만들어 필요한 부분을 교체해주는 방식을 취한다.
    
    업데이트가 필요한 곳에서는 아예 새로운 배열 혹은 새로운 객체를 만들기
    때문에, React.memo를 사용했을 때 props가 바뀌었는지 혹은 바뀌지
    않았는지를 알아내서 리렌더링 성능을 최적화해 줄 수 있다.
    
    이렇게 기존의 값을 직접 수정하지 않고 새로운 값을 만드는 것을 '불변성을
    지킨다' 고 한다.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import React, { useReducer, useRef, useCallback } from 'react';
+import React, { useState, useRef, useCallback } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
-import { dispatch } from '../../../Library/Caches/typescript/3.8/node_modules/rxjs/internal/observable/pairs';
 
 function createBulkTodos() {
   const array = [];
@@ -16,26 +15,8 @@ function createBulkTodos() {
   return array;
 }
 
-function todoReducer(todos, action) {
-  switch (action.type) {
-    case 'INSERT':
-      return todos.concat(action.todo);
-    case 'REMOVE':
-      return todos.filter(todo => todo.id !== action.id);
-    case 'TOGGLE':
-      return todos.map(todo =>
-        todo.id === action.id ? { ...todo, checked: !todo.checked } : todo,
-      );
-    default:
-      return todos;
-  }
-}
-
 const App = () => {
-  // useReducer를 사용할 때는 원래 두 번째 파라미터에 초기 상태를 넣어 주어야 함.
-  // 현재는 undefined를 넣고 세 번째 파라미터에 초기 상태를 만들어 주는 함수를 넣음.
-  // 이렇게 하면 컴포넌트가 맨 처음 렌더링될 때만 createBulkTodos 함수가 호출됨.
-  const [todos, dispatch] = useReducer(todoReducer, undefined, createBulkTodos);
+  const [todos, setTodos] = useState(createBulkTodos);
 
   //고윳값으로 사용될 id
   //ref를 사용하여 변수 담기
@@ -47,16 +28,20 @@ const App = () => {
       text,
       checked: false,
     };
-    dispatch({ type: 'INSERT', todo });
+    setTodos(todos => todos.concat(todo));
     nextId.current += 1;
   }, []);
 
   const onRemove = useCallback(id => {
-    dispatch({ type: 'REMOVE', id });
+    setTodos(todos => todos.filter(todo => todo.id !== id));
   }, []);
 
   const onToggle = useCallback(id => {
-    dispatch({ type: 'TOGGLE', id });
+    setTodos(todos =>
+      todos.map(todo =>
+        todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+      ),
+    );
   }, []);
 
   return (
